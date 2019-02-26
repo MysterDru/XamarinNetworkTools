ca8e18fcb6bad8bb49b62ddc51d0537a0f24f6cc
Drew Frisk <hello@drewfrisk.com>
2019-02-26 10:36:30 -0600

    java string split instead

--- libs/AndroidNetworkTools/ARPInfo.cs
+++ libs/AndroidNetworkTools/ARPInfo.cs
@@ -41,7 +41,8 @@ namespace AndroidNetworkTools
 
 			foreach (string line in getLinesInARPCache())
 			{
-				string[] splitted = line.Split(separator: (string)" +", options: StringSplitOptions.None);
+				var js = new Java.Lang.String(line);
+				string[] splitted = js.Split(" +")
 				if (splitted.Length >= 4 && string.Equals(ip, splitted[0]))
 				{
 					string mac = splitted[3];
@@ -80,7 +81,8 @@ namespace AndroidNetworkTools
 
 			foreach(string line in getLinesInARPCache())
 			{
-				string[] splitted = line.Split(separator: (string)" +", options: StringSplitOptions.None);
+				var js = new Java.Lang.String(line);
+				string[] splitted = js.Split(" +")
 				if (splitted.Length >= 4 && string.Equals(macAddress, splitted[3]))
 				{
 					return splitted[0];
